61cd7bb696e26c349d069846d3852eedd450760b
jameswang <james.wang.dev712@gmail.com>
2022-01-24 23:02:17 +0200

    Fix linter errors

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const body = document.body;
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelectorAll(".nav-list-link");
+const { body } = document;
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelectorAll('.nav-list-link');
 
-hamburger.addEventListener("click", () => {
-  body.classList.toggle("nav-open");
+hamburger.addEventListener('click', () => {
+  body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link => 
-  link.addEventListener('click', () => {
-    body.classList.remove('nav-open');
-  })
-);
+navLinks.forEach((link) => link.addEventListener('click', () => {
+  body.classList.remove('nav-open');
+}));
 
 document.addEventListener('scroll', () => {
   body.classList.remove('nav-open');
-})
+});
 
-let device = "";
+let device = '';
 const deviceType = () => {
   const ua = navigator.userAgent;
   if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-    device = "tablet";  
+    device = 'tablet';
     return device;
   }
-  else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-    device = "mobile";  
+  if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    device = 'mobile';
     return device;
   }
-  return "desktop";
+  return 'desktop';
 };
-deviceType()
-if(device === "mobile") {
-  document.getElementById("message-icon").textContent = "Contact";
+deviceType();
+if (device === 'mobile') {
+  document.getElementById('message-icon').textContent = 'Contact';
 }
