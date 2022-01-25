4cbe296a3f8fa12362ee90191e7066478b3b9925
jameswang <james.wang.dev712@gmail.com>
2022-01-25 16:58:18 +0200

    Add the mockup of the popup window

---
+++ projects.js
@@ -0,0 +1,12 @@
+const openButtons = document.querySelectorAll(".project-btn");
+const closeButton = document.querySelector('.pop-article-close');
+
+openButtons.forEach(button => 
+  button.addEventListener('click', () => 
+    document.body.classList.add('pop-open')
+  )
+);
+
+closeButton.addEventListener('click', () =>
+  document.body.classList.remove('pop-open')
+);
