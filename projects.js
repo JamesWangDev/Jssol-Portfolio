d6b30f91f87cb73ed214cd3d3effc6c4000d5c0f
ayubch <ayoub.chahir96@gmail.com>
2022-01-25 16:27:06 +0100

    Add projects.js file

--- projects.js
+++ projects.js
@@ -1,12 +1,6 @@
-const openButtons = document.querySelectorAll(".project-btn");
+const openButtons = document.querySelectorAll('.project-btn');
 const closeButton = document.querySelector('.pop-article-close');
 
-openButtons.forEach(button => 
-  button.addEventListener('click', () => 
-    document.body.classList.add('pop-open')
-  )
-);
+openButtons.forEach((button) => button.addEventListener('click', () => document.body.classList.add('pop-open')));
 
-closeButton.addEventListener('click', () =>
-  document.body.classList.remove('pop-open')
-);
+closeButton.addEventListener('click', () => document.body.classList.remove('pop-open'));
