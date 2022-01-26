f82b4419fed5d980a40bd0a06123deb32bb44f5f
jameswang <james.wang.dev712@gmail.com>
2022-01-26 19:51:57 +0200

    Add form validation

--- form.js
+++ form.js
@@ -1,10 +1,27 @@
 const form = document.getElementById('contact-me');
+const msg = document.getElementById('error');
+const mail = form.elements["email"];
 
 function isLowerCase(string) {
-  if(string !== string.toLowerCase()) {
-      console.log('has upper')
+  return (string !== string.toLowerCase()) ? false : true;
+};
+
+function showError() {
+  msg.style.display = 'block';
+  msg.innerText = 'Email must be lowercase';
+}
+
+form.addEventListener('submit', (event) => {
+  let result = isLowerCase(mail.value);
+  if(!result) {
+    event.preventDefault();
+    showError();
   }
   else {
-      console.log('has not upper')
+    form.submit();
   }
-};
+});
+
+mail.addEventListener('input', () => {
+  msg.style.display = 'none';
+});
