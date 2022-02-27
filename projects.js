22b72a67a7ce22ef7485b783cf78e55249954d73
jameswang <james.wang.dev712@gmail.com>
2022-02-27 17:43:26 +0200

    Create the popup item

--- projects.js
+++ projects.js
@@ -3,7 +3,6 @@
 // const technologies = Array.from(document.querySelectorAll('.techno'));
 // const live = document.getElementById('live');
 // const source = document.getElementById('source');
-// const openButtons = document.querySelectorAll('.project-btn');
 // const closeButton = document.querySelector('.pop-article-close');
 const projectSection = document.getElementById('projects');
 const projects = [
@@ -45,7 +44,71 @@ const projects = [
   },
 ];
 
-function setContent() {
+const showPop = (identifier) => {
+  const checker = identifier.slice(0, identifier.length - 4);
+  const popArticle = document.createElement('article');
+  projects.forEach((project) => {
+    if (project.name === checker) {
+      const popHeader = document.createElement('div');
+      const popArticleTitle = document.createElement('h2');
+      const popCloseBtn = document.createElement('button');
+      const popIllustration = document.createElement('div');
+      const popDescription = document.createElement('p');
+      const techstack = document.createElement('ul');
+      const links = document.createElement('div');
+      const liveBtn = document.createComment('a');
+      const sourceBtn = document.createComment('a');
+
+      popArticle.classList.add('pop-up', 'pop-article');
+      popHeader.className = 'pop-article-head';
+      popArticleTitle.className = 'pop-article-title';
+      popArticleTitle.textContent = project.title;
+      popCloseBtn.className = 'pop-article-close';
+      popCloseBtn.innerHTML = '<span class="pop-close"></span>';
+      popIllustration.className = 'project-illustration';
+      popIllustration.style.backgroundImage = `url(${project.image})`;
+      popDescription.className = 'project-description';
+      popDescription.textContent = `Lorem Ipsum is simply dummy text 
+      of the printing and typesetting 
+      industry. Lorem Ipsum has been the 
+      industry's standard dummy text ever 
+      since the 1500s, when an unknown 
+      printer took a galley of type and 
+      scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy 
+      text of the printing and typesetting  
+      ever since the 1500s, when an unknown 
+      printer took a galley of type veris lapoa todoe.`;
+      techstack.className = 'techstack';
+      project.technologies.forEach((tech) => {
+        const li = document.createElement('li');
+        li.className = 'techno';
+        li.textContent = tech;
+        techstack.appendChild(li);
+      });
+      links.className = 'project-links';
+      links.innerHTML += `<a target="_blank" href='${project.live}' class="live" id="live">See Live <i class="fas fa-external-link-alt brand" aria-hidden="true"></i></a>
+      <a target="_blank" href='${project.source}' class="source" id="source">See Source <i class="fab fa-github brand" aria-hidden="true"></i></a>`;
+
+      popHeader.append(popArticleTitle, popCloseBtn);
+      popArticle.append(popHeader, popIllustration, popDescription, techstack, links);
+      popArticle.addEventListener('mouseup', (e) => {
+        e.stopPropagation();
+        document.body.classList.remove('pop-open');
+        document.body.removeChild(popArticle);
+      });
+      popCloseBtn.addEventListener('click', () => {
+        document.body.classList.remove('pop-open');
+        document.body.removeChild(popArticle);
+      });
+    }
+  });
+  document.body.appendChild(popArticle);
+  document.body.classList.add('pop-open');
+  popArticle.scrollIntoView(true);
+};
+
+const setContent = () => {
   projects.forEach((project) => {
   const article = `<article class="project-card" id=${project.name}>
   <section class="image-placeholder">
@@ -64,21 +127,18 @@ function setContent() {
     <li class="technology">${project.technologies[2]}</li>
     <li class="technology">${project.technologies[3]}</li>
   </ul>
-  <button type="button" class="button project-btn">See Project</button>
+  <button type="button" id='${project.name}-btn' class="button project-btn">See Project</button>
   </section>
   </article>`;
 
+  document.addEventListener('click',function(e){
+    if(e.target && e.target.id === `${project.name}-btn`){
+      showPop(`${project.name}-btn`);
+    }
+  });
   projectSection.innerHTML += article;
   });
 }
-
-// closeButton.addEventListener('click', () => {
-//   document.body.classList.remove('pop-open');
-// });
-// document.querySelector('.pop-article').addEventListener('mouseup', (event) => {
-//   document.body.classList.remove('pop-open');
-//   event.stopPropagation();
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
   setContent();
