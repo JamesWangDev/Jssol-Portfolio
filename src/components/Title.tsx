14caef8bcad6c933fa0999ab4f3124077c4ba74b
jameswang <james.wang.dev712@gmail.com>
2023-03-04 01:27:57 +0200

    Build custom Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = ({ index, title, subtitle }) => {
+  return (
+    <section className='title_container'>
+      <h2 className='section_title'>{title}</h2>
+      <div className='upper_div'>
+        <p className='title_index'>{index}</p>
+        <p className='section_subtitle'>{subtitle}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Title;
