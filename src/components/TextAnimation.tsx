16605f078595094b6b8c041e70ea8c66f7bc197a
jameswang <james.wang.dev712@gmail.com>
2023-03-10 00:51:56 +0200

    Refactor to fix max dependency calls issue

--- src/components/TextAnimation.tsx
+++ src/components/TextAnimation.tsx
@@ -16,59 +16,53 @@ const TextAnimation: React.FC<Props> = ({
   type, delay, children, className = '', threshold = 0.5,
 }) => {
   const [ref, inView] = useInView({ threshold }); // detect when element is 50% in viewport
-  const [animation, setAnimation] = useState({});
 
-  const fadeUp = useSpring({
-    opacity: inView ? 1 : 0, // animate opacity from 0 to 1 when in viewport
-    transform: inView ? 'translateY(0%)' : 'translateY(-3%)', // animate translateX from -100% to 0% when in viewport
-    config: { duration: 300 }, // animation duration in milliseconds
-    delay: 100 * delay, // delay animation by 250 milliseconds
-  });
-
-  const fadeDown = useSpring({
-    opacity: inView ? 1 : 0, // animate opacity from 0 to 1 when in viewport
-    transform: inView ? 'translateY(0%)' : 'translateY(3%)', // animate translateX from -100% to 0% when in viewport
-    config: { duration: 300 }, // animation duration in milliseconds
-    delay: 100 * delay, // delay animation by 250 milliseconds
-  });
-
-  const fadeRight = useSpring({
-    opacity: inView ? 1 : 0, // animate opacity from 0 to 1 when in viewport
-    transform: inView ? 'translateX(0%)' : 'translateX(-3%)', // animate translateX from -100% to 0% when in viewport
-    config: { duration: 300 }, // animation duration in milliseconds
-    delay: 100 * delay, // delay animation by 250 milliseconds
-  });
-
-  const fadeLeft = useSpring({
-    opacity: inView ? 1 : 0, // animate opacity from 0 to 1 when in viewport
-    transform: inView ? 'translateX(0%)' : 'translateX(3%)', // animate translateX from -100% to 0% when in viewport
-    config: { duration: 300 }, // animation duration in milliseconds
-    delay: 100 * delay, // delay animation by 250 milliseconds
-  });
-
-  useEffect(() => {
+  const getAnimation = () => {
     switch (type) {
       case 'fade_up':
-        setAnimation(fadeUp);
-        break;
+        return {
+          opacity: inView ? 1 : 0,
+          transform: inView ? 'translateY(0%)' : 'translateY(-3%)',
+          config: { duration: 300 },
+          delay: 100 * delay,
+        };
       case 'fade_down':
-        setAnimation(fadeDown);
-        break;
+        return {
+          opacity: inView ? 1 : 0,
+          transform: inView ? 'translateY(0%)' : 'translateY(3%)',
+          config: { duration: 300 },
+          delay: 100 * delay,
+        };
       case 'fade_right':
-        setAnimation(fadeRight);
-        break;
+        return {
+          opacity: inView ? 1 : 0,
+          transform: inView ? 'translateX(0%)' : 'translateX(-3%)',
+          config: { duration: 300 },
+          delay: 100 * delay,
+        };
       case 'fade_left':
-        setAnimation(fadeLeft);
-        break;
+        return {
+          opacity: inView ? 1 : 0,
+          transform: inView ? 'translateX(0%)' : 'translateX(3%)',
+          config: { duration: 300 },
+          delay: 100 * delay,
+        };
       default:
-        setAnimation(fadeDown);
+        return {
+          opacity: inView ? 1 : 0,
+          transform: inView ? 'translateY(0%)' : 'translateY(3%)',
+          config: { duration: 300 },
+          delay: 100 * delay,
+        };
     }
-  }, [type, fadeUp, fadeDown, fadeRight, fadeLeft, setAnimation]);
+  };
+
+  const animation = useSpring(getAnimation());
 
   return (
-    <animated.div className={className} ref={ref} style={animation}>
+    <animated.span className={className} ref={ref} style={animation}>
       {children}
-    </animated.div>
+    </animated.span>
   );
 };
 
