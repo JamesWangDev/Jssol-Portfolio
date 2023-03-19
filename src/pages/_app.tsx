13cc349b6254f348b9953e8e77c311c0026bb92b
jameswang <james.wang.dev712@gmail.com>
2023-03-19 05:06:27 +0200

    Fix linters

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '@/styles/index.scss';
 import type { AppProps } from 'next/app';
-import { Playfair_Display, Poppins } from '@next/font/google';
+import { Playfair_Display as PlayfairDisplay, Poppins } from '@next/font/google';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
-const playfairDisplay = Playfair_Display({
+const playfairDisplay = PlayfairDisplay({
   subsets: ['latin'],
   weight: ['700'],
 });
